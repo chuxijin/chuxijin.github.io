51c0370a74c978dfb0bc8094ebda2b5728bd565d
chuxijin <76930682+chuxijin@users.noreply.github.com>
2021-02-26 16:38:37 +0800

    msg

---
+++ main/js/back.js
@@ -0,0 +1,31 @@
+window.history.go(-1);  //返回上一页
+window.history.back();  //返回上一页
+window.location.go(-1); //刷新上一页
+ 
+window.history.back();location.reload();//强行刷新(返回上一页刷新页面)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
